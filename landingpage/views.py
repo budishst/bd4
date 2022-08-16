56b41c4641be1041cd75f178f3dbeec742d4d034
budishst <53331116+budishst@users.noreply.github.com>
2022-08-16 08:50:04 +0700

    add request button

--- landingpage/views.py
+++ landingpage/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def home_view(request):
+ 
+    # logic of view will be implemented here
+    return render(request, "landingpage/templates/home.html")
