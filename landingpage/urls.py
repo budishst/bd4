392d569d1180c1fe069fd1196ddfda0bc19f6ee2
budishst <53331116+budishst@users.noreply.github.com>
2022-08-16 09:15:00 +0700

    a

--- landingpage/urls.py
+++ landingpage/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from . import landingpage.views
+from . import views
 
 urlpatterns = [
+<<<<<<< HEAD
+    path('', views.home_view, name='index'),
+=======
     path('', views.home_view, name='home_view'),
+>>>>>>> cf52d2e41c4820f4875974c1a046e3aacdae24e3
 ]
